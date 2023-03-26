094681e5a20f549eeac77339445c560c8724021a
michael <michael.mittoo@gmail.com>
2023-03-26 15:34:04 +0200

    add github and google auth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -4,6 +4,9 @@ import { useCallback, useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
 
+import {FcGoogle} from "react-icons/fc";
+import {FaGithub} from "react-icons/fa"
+
 const Auth = () => {
     const router = useRouter();
     const [email, setEmail] = useState("");
@@ -32,7 +35,7 @@ const Auth = () => {
         }
 
     },[email, password, router])
-    
+
     const register = useCallback( async () => {
         try{
             await axios.post("/api/register", {
@@ -86,6 +89,43 @@ const Auth = () => {
                         <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                             {variant === "login" ? "Login" : "Sign up"}
                         </button>
+
+                        <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                                <div 
+                                onClick={() => signIn("google", {callbackUrl: "/"})}
+                                className="
+                                    w-10
+                                    h-10
+                                    bg-white
+                                    rounded-full
+                                    flex
+                                    items-center
+                                    justify-center
+                                    cursor-pointer
+                                    hover:opacity-80
+                                    transition
+                                    "
+                                >
+                                    <FcGoogle size={30} />
+                                </div>
+                                <div 
+                                onClick={() => signIn("github", {callbackUrl: "/"})}
+                                className="
+                                    w-10
+                                    h-10
+                                    bg-white
+                                    rounded-full
+                                    flex
+                                    items-center
+                                    justify-center
+                                    cursor-pointer
+                                    hover:opacity-80
+                                    transition
+                                    "
+                                >
+                                    <FaGithub size={30} />
+                                </div>
+                        </div>
                         <p className="text-neutral-500 mt-12">
                             {variant === "login" ? "Don't have an account? " : "Already have an account? "}
                             <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
