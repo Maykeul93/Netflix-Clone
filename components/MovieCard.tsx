61ad7b9814600fab133f19d0ba8a3a49ef6870b3
michael <michael.mittoo@gmail.com>
2023-04-18 18:57:49 +0200

    create PlayButton

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { useRouter } from "next/router";
 import {BsFillPlayFill} from 'react-icons/bs';
 import FavoriteButton from "./FavoriteButton";
 
@@ -11,6 +11,9 @@ interface MovieCardProps {
 const MovieCard: React.FC<MovieCardProps> = ({
     data
 }) => {
+
+    const router = useRouter();
+
     return (
         <div className="group bg-zinc-900 col-span relative h-[12vw]">
             <img 
@@ -86,7 +89,7 @@ const MovieCard: React.FC<MovieCardProps> = ({
                             transition
                             hover:bg-neutral-300
                             "
-                            onClick={()=>{}}
+                            onClick={()=>{router.push(`/watch/${data?.id}`)}}
                             >
                                 <BsFillPlayFill size={30}/>
                             </div>
